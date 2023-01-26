8440262c240cc83d5980e3675f6b3e65b9ae1a7a
Karina Ganieva <karinag@lenta.ru>
2023-01-26 11:08:58 +0300

    add projects api

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Project;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function() {
+    return Project::all();
 });
